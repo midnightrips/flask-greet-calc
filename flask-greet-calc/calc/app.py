7d2a9ae625a278caee0feaee3b7e2fb089364efc
Ehretz Geskin <sg3584@alum.barnard.edu>
2024-08-09 11:33:46 -0400

    Completing Assignment

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -1,1 +1,41 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+app = Flask(__name__)
+
+@app.route('/add')
+def add():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(a + b)
+
+@app.route('/sub')
+def subtract():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(a - b)
+
+@app.route('/mult')
+def multiply():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(a * b)
+
+@app.route('/div')
+def divide():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(a / b)
+
+operators = {
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div
+}
+
+@app.route('/math/<oper>')
+def math_opers(oper):
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(operators[oper](a, b))
